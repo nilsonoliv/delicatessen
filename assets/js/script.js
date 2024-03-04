a6864fb2a2eaadd810a8b8ae4a48578fc9c7f5ae
nilsonoliv <nilsonespiritosant@gmail.com>
2024-03-04 13:47:48 -0300

    init

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+const navbar = document.querySelector(".navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+};
+
+const searchForm = document.querySelector(".search-form");
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+  navbar.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
+const cartItem = document.querySelector(".cart-items-container");
+
+document.querySelector("#cart-btn").onclick = () => {
+  cartItem.classList.toggle("active");
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+  cartItem.classList.remove("active");
+};
